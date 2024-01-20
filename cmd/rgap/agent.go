c4b1147a10055c58b4987806acab6e454b99a4d6
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2024-01-20 21:42:57 +0200

    agent CLI options

--- cmd/rgap/agent.go
+++ cmd/rgap/agent.go
@@ -11,28 +11,34 @@ import (
 )
 
 var (
-	group    uint64
-	address  net.IP
-	key      pskOption
-	interval time.Duration
+	group        uint64
+	address      net.IP
+	key          pskOption
+	interval     time.Duration
+	destinations []string
 )
 
 type pskOption struct {
 	psk *rgap.PSK
 }
 
-func (psk pskOption) String() string {
+func (psk *pskOption) String() string {
 	if psk.psk == nil {
 		return "<nil>"
 	}
 	return psk.psk.String()
 }
 
-func (psk pskOption) Set(s string) error {
-	return psk.psk.FromHexString(s)
+func (psk *pskOption) Set(s string) error {
+	newPSK := new(rgap.PSK)
+	if err := newPSK.FromHexString(s); err != nil {
+		return err
+	}
+	psk.psk = newPSK
+	return nil
 }
 
-func (psk pskOption) Type() string {
+func (psk *pskOption) Type() string {
 	return "hexstring"
 }
 
@@ -41,7 +47,7 @@ var agentCmd = &cobra.Command{
 	Use:   "agent",
 	Short: "Run agent to send announcements",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(group, address, key.psk, interval)
+		fmt.Println(group, address, key.psk, interval, destinations)
 	},
 }
 
@@ -60,4 +66,6 @@ func init() {
 	agentCmd.Flags().Uint64VarP(&group, "group", "g", 0, "redundancy group")
 	agentCmd.Flags().IPVarP(&address, "address", "a", nil, "IP address to announce")
 	agentCmd.Flags().VarP(&key, "psk", "k", "pre-shared key for announcement signature")
+	agentCmd.Flags().DurationVarP(&interval, "interval", "i", 0, "announcement interval. If not specified agent sends one announce and exits")
+	agentCmd.Flags().StringArrayVarP(&destinations, "dst", "d", nil, "announcement destination address:port. Can be specified multiple times")
 }
