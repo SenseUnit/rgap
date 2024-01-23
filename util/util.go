56dfeb6acdaab16bc916eba99a7125e5cff6ee98
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2024-01-23 13:12:30 +0200

    server support for custom multicast interface

--- util/util.go
+++ util/util.go
@@ -2,8 +2,12 @@ package util
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"log"
+	"net"
 	"net/netip"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -57,4 +61,59 @@ func CheckedUnmarshal(doc *yaml.Node, dst interface{}) error {
 		return fmt.Errorf("unable to unmarshal node: %w", err)
 	}
 	return nil
+}
+
+func SplitAndResolveAddrSpec(spec string) (string, *net.Interface, error) {
+	addrSpec, ifaceSpec, found := strings.Cut(spec, "@")
+	if !found {
+		return addrSpec, nil, nil
+	}
+	iface, err := ResolveInterface(ifaceSpec)
+	if err != nil {
+		return addrSpec, nil, fmt.Errorf("unable to resolve interface spec %q: %w", ifaceSpec, err)
+	}
+	return addrSpec, iface, nil
+}
+
+func ResolveInterface(spec string) (*net.Interface, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, fmt.Errorf("unable to enumerate interfaces: %w", err)
+	}
+	if pfx, err := netip.ParsePrefix(spec); err == nil {
+		// look for address
+		for i := range ifaces {
+			addrs, err := ifaces[i].Addrs()
+			if err != nil {
+				// may be a problem with some interface,
+				// but we still probably can find the right one
+				log.Printf("WARNING: interface %s is failing to report its addresses: %v", ifaces[i].Name, err)
+				continue
+			}
+			for _, addr := range addrs {
+				ipnet, ok := addr.(*net.IPNet)
+				if !ok {
+					return nil, fmt.Errorf("unexpected type returned as address interface: %T", addr)
+				}
+				netipAddr, ok := netip.AddrFromSlice(ipnet.IP)
+				if !ok {
+					return nil, fmt.Errorf("interface %v has invalid address %s", ifaces[i].Name, ipnet.IP)
+				}
+				netipAddr = netipAddr.Unmap()
+				if pfx.Contains(netipAddr) {
+					res := ifaces[i]
+					return &res, nil
+				}
+			}
+		}
+	} else {
+		// look for iface name
+		for i := range ifaces {
+			if ifaces[i].Name == spec {
+				res := ifaces[i]
+				return &res, nil
+			}
+		}
+	}
+	return nil, errors.New("specified interface not found")
 }
